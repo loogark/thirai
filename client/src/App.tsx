8566f37e5c1ce27afff7795e7e4758cfc1bfa5f9
loogark <ragoolkrishnan.ram@gmail.com>
2024-02-19 02:09:31 +0100

    Add navigation to home page in App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Outlet, ScrollRestoration } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, ScrollRestoration, useNavigate } from "react-router-dom";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { AuthModalProvider } from "./context/AuthModalProvider";
@@ -10,6 +11,11 @@ import { createTheme } from "./utils/theme";
 
 function App() {
   const proTheme = createTheme();
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate("/home");
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <ChakraProvider theme={proTheme}>
