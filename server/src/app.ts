30ec095c568ee0be6c0dea879f29de7263afee0a
loogark <ragoolkrishnan.ram@gmail.com>
2024-02-20 21:35:40 +0100

    Update CORS origin to use production URL

--- server/src/app.ts
+++ server/src/app.ts
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://thirai.vercel.app/",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
