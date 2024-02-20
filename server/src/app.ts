cac9f6470ae46415e8f2ac1255f3820f26a27dc0
loogark <ragoolkrishnan.ram@gmail.com>
2024-02-20 21:40:15 +0100

    Update CORS origin URL in app.js and app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://thirai.vercel.app/",
+    origin: "https://thirai.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
